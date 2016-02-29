f7ed2b6e22dbfb941a5e891c526b7da460ca4832
Ethan <erdesi90562@gmail.com>
2016-02-29 12:37:35 -0500

    minimal code json items to front page.

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,3 +1,30 @@
 'use strict';
 
+const api = require('./api-requests/api.js');
+
+$( document ).ready(function() {
+  $('.listname').text('List Name!');
+  $('.listbox').text('listbox');
+  $('.item').text('listsbox');
+  getAll();
+
+  // user require with a reference to bundle the file and use it in this file
+  // let example = require('./example');
+});
+
+let getAll = function () {
+  $.ajax({
+    url: "http://localhost:3000/items",
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=951473cfb9adf638cb12eb994c2679e3',
+    },
+    dataType: 'json'
+  }).done(function(data) {
+    let template = require('./handlebars/list-item.handlebars');
+    $('.dog').append(template({data}));
+    console.log(data);
+  });
+};
+
 module.exports = true;
