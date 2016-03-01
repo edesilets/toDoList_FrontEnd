9416d0d604c0d360f2a26218c26fdb44b7121529
Ethan <erdesi90562@gmail.com>
2016-03-01 09:21:47 -0500

    create basic list setup
    use a better method to get list information.
    change the index.html and list-wrapper.handlebars to have a working state.

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,28 +1,46 @@
 'use strict';
-
+// Require js files below
 const api = require('./api-requests/api.js');
+
+// Require handlebars files below
 
 $( document ).ready(function() {
   $('.listname').text('List Name!');
   $('.listbox').text('listbox');
   $('.item').text('listsbox');
-  getAll();
+  //getItems();
+  getLists();
 
   // user require with a reference to bundle the file and use it in this file
   // let example = require('./example');
 });
 
-let getAll = function () {
+let getItems = function () {
   $.ajax({
     url: "http://localhost:3000/items",
     method: 'GET',
     headers: {
-      Authorization: 'Token token=951473cfb9adf638cb12eb994c2679e3',
+      Authorization: 'Token token=7afe163c52a8ad3381dae7008f56fb6f',
     },
     dataType: 'json'
   }).done(function(data) {
     let template = require('./handlebars/list-item.handlebars');
     $('.dog').append(template({data}));
+    console.log(data);
+  });
+};
+
+let getLists = function () {
+  $.ajax({
+    url: "http://localhost:3000/lists",
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=7afe163c52a8ad3381dae7008f56fb6f',
+    },
+    dataType: 'json'
+  }).done(function(data) {
+    let listsTemplate = require('./handlebars/list-wrapper.handlebars');
+    $('.listwrapper').append(listsTemplate({data}));
     console.log(data);
   });
 };
