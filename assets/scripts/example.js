7097717af0b114bc15e756535b4ac7373b34b0c6
Ethan <erdesi90562@gmail.com>
2016-03-01 18:42:29 -0500

    relocate ajax request for get items and lists to api.js

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -10,42 +10,7 @@ $( document ).ready(function() {
   $('.listname').text('List Name!');
   $('.listbox').text('listbox');
   $('.item').text('listsbox');
-  //getItems();
-  getLists();
   $('.navbar').append(navbarTemplate());
-
-  // user require with a reference to bundle the file and use it in this file
-  // let example = require('./example');
 });
-
-let getItems = function () {
-  $.ajax({
-    url: "http://localhost:3000/items",
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=7afe163c52a8ad3381dae7008f56fb6f',
-    },
-    dataType: 'json'
-  }).done(function(data) {
-    let template = require('./handlebars/list-item.handlebars');
-    $('.dog').append(template({data}));
-    console.log(data);
-  });
-};
-
-let getLists = function () {
-  $.ajax({
-    url: "http://localhost:3000/lists",
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=7afe163c52a8ad3381dae7008f56fb6f',
-    },
-    dataType: 'json'
-  }).done(function(data) {
-    let listsTemplate = require('./handlebars/list-wrapper.handlebars');
-    $('.listwrapper').append(listsTemplate({data}));
-    console.log(data);
-  });
-};
 
 module.exports = true;
