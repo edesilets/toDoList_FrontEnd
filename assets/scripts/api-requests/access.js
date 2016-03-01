4cae1c219f5939315f75f87bae56c0310a40bf00
Ethan <erdesi90562@gmail.com>
2016-03-01 15:06:21 -0500

    add logout ajax request

--- assets/scripts/api-requests/access.js
+++ assets/scripts/api-requests/access.js
@@ -52,9 +52,32 @@ let logIn = function(event) {
   });
 };
 
+let logOut = function(event) {
+  event.preventDefault();                // Stops page Reload
+  let item = new FormData(event.target);    // object containing the FormData
+  console.log(item);
+  $.ajax({
+    url: env.envVars.url + 'sign-out/' + env.envVars.user.user.id,
+    type: 'DELETE',
+    headers: {
+        Authorization: 'Token token=' + env.envVars.user.user.token,
+      },
+    contentType: false,                     // Needed for FormData
+    processData: false,                     // Needed for FormData This is because item
+    data: item                              // item is referancing the new object called 'item'.
+  })
+  .done(function (data) {
+    successMessage(data);
+  })
+  .fail(function (data) {
+    failMessage(data);
+  });
+};
+
+
 module.exports = {
   signUp,
   logIn,
-  // logOut,
+  logOut,
   // chPass,
 };
